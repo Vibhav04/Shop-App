76aea48f1117d0ead5b957dfa4df7e0f9598d81a
Vibhav04 <vibhav.sinha@hotmail.com>
2022-07-19 22:04:40 +0530

    updating listener

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:myshop_app/providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
   const ProductDetailScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'title',
+        title: Text(
+          loadedProduct.title,
         ),
       ),
     );
